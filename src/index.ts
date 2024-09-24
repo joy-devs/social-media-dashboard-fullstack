69d41c3f923febdee21af246eff97b1703207c93
Joyce Wambui <joycewambui@gmail.com>
2024-09-24 16:59:43 +0300

    added routes

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,50 @@
 import { serve } from "@hono/node-server";
 import { Hono } from 'hono';
 import "dotenv/config";
-import {usersRouter} from "./users/user.router";
-import{postsRouter} from "./posts/post.router";
-import {followersRouter} from "./follows/follow.router";
+import { usersRouter } from "./users/user.router"; 
+import {postsRouter} from "./posts/post.router";
 import {commentsRouter} from "./comments/comment.router";
+import { followersRouter } from "./follows/follow.router";
+
+
+import { cors } from 'hono/cors'
 
 const app = new Hono();
-app.use()
+app.use(
+  '*',
+  cors({
+    origin: '*',
+    allowMethods: ['POST', 'GET', 'PUT', 'DELETE']
+  })
+)
+
+ 
+
+
 
 //custom route
 app.route("/api", usersRouter)
 app.route("/api", postsRouter)
+app.route("/api", commentsRouter )
 app.route("/api", followersRouter)
-app.route("/api", commentsRouter)
-
-app.get("/" , (c) => {
-    return c.text("The server is running")
-});
 
 
-app.notFound((c) => {
+app.get("/", (c) => {
+    return c.text('The server is running');
+  });
+  
+  export default app;
+  
+  app.notFound((c) => {
     return c.text('route not found')
-})
-
-const PORT = process.env.PORT || 8000;
-console.log(`server is running on port ${PORT}`);
-
-serve({
-    fetch:app.fetch.bind(app),
+  })
+  
+  const PORT = process.env.PORT || 8000; 
+  
+  console.log(`Server is running on port ${PORT}`);
+  
+  serve({
+    fetch: app.fetch.bind(app),
     port: Number(PORT)
-});
-
-export default app;
+  });
+  
